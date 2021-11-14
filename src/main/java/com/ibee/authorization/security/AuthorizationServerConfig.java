6997f443917ecd4fb29322002c580cd301154e42
Jefferson Alves <jeffersonalmeida16@outlook.com>
2021-11-14 08:40:15 -0300

    initials configs

---
+++ src/main/java/com/ibee/authorization/security/AuthorizationServerConfig.java
@@ -0,0 +1,13 @@
+package com.ibee.authorization.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter  {
+
+}
---
+++ src/main/java/com/ibee/authorization/security/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.ibee.authorization.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    private final String USER = "jefalves@gmail.com";
+    private final String PASSWORD = "123456789";
+    private final String ADMIN = "Admin";
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(USER)
+                .password(passwordEncoder.encode(PASSWORD))
+                .roles(ADMIN);
+    }
+
+}
