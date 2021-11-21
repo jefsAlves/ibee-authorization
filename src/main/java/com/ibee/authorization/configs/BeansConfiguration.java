f3385967a66c1937277c1f70416370de86298205
Jefferson Alves <jeffersonalmeida16@outlook.com>
2021-11-21 16:48:05 -0300

    add key asymmetric

--- src/main/java/com/ibee/authorization/configs/BeansConfiguration.java
+++ src/main/java/com/ibee/authorization/configs/BeansConfiguration.java
@@ -1,14 +1,25 @@
 package com.ibee.authorization.configs;
 
-import com.ibee.authorization.security.JwtMessage;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 @Configuration
 public class BeansConfiguration {
+
+    @Value("${ibee.jwt.keystore.path}")
+    private ClassPathResource resourceJks;
+
+    @Value("${ibee.jwt.keystore.password}")
+    private String passwordResourceJks;
+
+    @Value("${ibee.jwt.keystore.alias}")
+    private String aliasKeyPair;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -18,7 +29,15 @@ public class BeansConfiguration {
     @Bean
     public JwtAccessTokenConverter jwtAccessTokenConverter() {
         JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-        jwtAccessTokenConverter.setSigningKey(JwtMessage.SECRET_JWT);
+
+        var path = resourceJks;
+        var password = passwordResourceJks;
+        var alias = aliasKeyPair;
+
+        var file = new KeyStoreKeyFactory(path, password.toCharArray());
+        var open = file.getKeyPair(alias);
+        jwtAccessTokenConverter.setKeyPair(open);
+
         return jwtAccessTokenConverter;
     }
 
---
+++ src/main/java/com/ibee/authorization/security/JWTUtil.java
@@ -0,0 +1,12 @@
+package com.ibee.authorization.security;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+
+import java.security.KeyPair;
+
+public class JWTUtil {
+
+    public static final String SECRET_JWT = "JsiSx72lxcV2D4ls8jzXns93ZkIow3snQ9cbZ1d5b7g4af2D9hc4pzA4h2fwAo84hc39G";
+
+}
